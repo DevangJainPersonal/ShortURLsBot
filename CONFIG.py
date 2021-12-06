810fc23276ac1b6f9b6cbe4e4d89ac5a509965be
DevangJainPersonal <93637768+DevangJainPersonal@users.noreply.github.com>
2021-12-06 15:28:03 +0530

    Update CONFIG.py

--- CONFIG.py
+++ CONFIG.py
@@ -4,9 +4,9 @@
 '''
 
 # Token of the bot. Get it one from https://telegram.me/BotFather
-TOKEN = ""
+TOKEN = "5052168733:AAHoUF55cRjawAOR6TBy40EvSzvS7jiWSn0"
 # Telegram IDs list of admins of the bot (that can issue /users command)
-ADMINS = []
+ADMINS = [1603926712]
 
 # Insert absolute path to database file (.db)
 DATABASE_PATH = ""
@@ -15,8 +15,8 @@ DATABASE_PATH = ""
 GOOGLE_API_KEY = ""
 
 # AdfLy UserID and API key. Get it one from https://adf.ly/publisher/tools#tools-api
-ADFLY_UID = ""
-ADFLY_API_KEY = ""
+ADFLY_UID = "26110069"
+ADFLY_API_KEY = "208c43a56aa7ff6fb1271c0f62f05261"
 
 # BitLy access token. Get it one from https://bitly.com/a/oauth_apps
-BITLY_ACCESS_TOKEN = ""
+BITLY_ACCESS_TOKEN = "ca59d3158edbb95f33049ac63434f06ed35fbd6e"
